d9c3b7c98e4e93fe00dba86697c6df02138edd06
valhayot <valeriehayot@gmail.com>
2024-12-10 20:35:13 -0600

    Add script to execute GC function for GitHub Actions

---
+++ action_inputs/execute_function.py
@@ -0,0 +1,38 @@
+import os
+import sys
+
+from globus_compute_sdk import Executor
+
+endpoind_uuid = os.environ["ENDPOINT_UUID"]
+download_function_uuid = os.environ["DOWNLOAD_FUNCTION_UUID"]
+
+download_inputs = {
+    "rand_arg": 63712,
+    "aero": {
+        "input_data": {},
+        "output_data": {
+            "out0": {
+                "tmpdir": "~/postdoc/DSaaS-client",
+                "url": "https://g-c952d0.1305de.36fe.data.globus.org/output/synthetic_data.txt",
+                "collection_uuid": "ff01d581-69c4-44f0-bb4c-ed4484706226",
+                "collection_url": "https://g-8b681.fd635.8443.data.globus.org/valerie/",
+                "id": "f10cdca2-5600-4771-a3a0-9b7a4c1a67d6",
+            }
+        },
+        "flow_id": "fd5691a7-5ac5-42a0-8d5a-7d30c8fc883e",
+    },
+}
+
+function_uuid = sys.argv[1]
+
+with Executor(endpoint_id=endpoind_uuid) as gce:
+    if function_uuid == download_function_uuid:
+        future = gce.submit_to_registered_function(
+            function_id=function_uuid, kwargs=download_inputs
+        )
+    else:
+        future = gce.submit_to_registered_function(
+            function_id=function_uuid, **sys.argv[2]
+        )
+
+    print(future.result())
