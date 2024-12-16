d8a9aa4f7c50bf6a291fdaf607a7ea67f06cec34
Valerie Hayot-Sasson <ValHayot@users.noreply.github.com>
2024-12-16 11:30:08 -0600

    Update execute_function.py

--- action_inputs/execute_function.py
+++ action_inputs/execute_function.py
@@ -8,7 +8,7 @@ download_function_uuid = os.environ["DOWNLOAD_FUNCTION_UUID"]
 custom_function_uuid = os.environ["CUSTOM_FUNCTION_UUID"]
 
 download_inputs = {
-    "rand_arg": 284068800,
+    "rand_arg": 63712,
     "metrics": True,
     "aero": {
         "input_data": {},
@@ -25,6 +25,40 @@ download_inputs = {
     },
 }
 
+args = [
+    [],
+    {
+        "aero": {
+            "flow_id": "d9bf4e2d-5b32-4c24-88a7-2fe1dd4d6628",
+            "input_data": {},
+            "output_data": {
+                "out0": {
+                    "checksum": "7818c9cba4fbfac4b6e3d8802397ae2e",
+                    "collection_url": "https://g-8b681.fd635.8443.data.globus.org/valerie/",
+                    "collection_uuid": "ff01d581-69c4-44f0-bb4c-ed4484706226",
+                    "download": True,
+                    "encoding": "UTF-8",
+                    "file": "/Users/valeriehayot-sasson/aero/875ba579-7531-4537-aaa1-a630174935eb",
+                    "file_bn": "875ba579-7531-4537-aaa1-a630174935eb",
+                    "file_format": ".txt",
+                    "id": "3e4e744f-1a6c-4f7d-912f-79e9f566ad98",
+                    "size": 61865984,
+                    "temp_dir": "/Users/valeriehayot-sasson/aero",
+                    "tmpdir": "~/postdoc/DSaaS-client",
+                    "url": "https://g-c952d0.1305de.36fe.data.globus.org/output/synthetic_data.txt",
+                }
+            },
+        },
+        "download_metrics": {
+            "duration": 13028524000,
+            "task_end": 1734321528873406000,
+            "task_start": 1734321515844882000,
+        },
+        "metrics": True,
+        "rand_arg": 284068800,
+    },
+]
+
 function_uuid = sys.argv[1]
 
 with Executor(endpoint_id=endpoind_uuid) as gce:
@@ -34,7 +68,7 @@ with Executor(endpoint_id=endpoind_uuid) as gce:
         )
     elif function_uuid == custom_function_uuid:
         future = gce.submit_to_registered_function(
-            function_id=function_uuid #, kwargs=eval(sys.argv[2])[1]
+            function_id=function_uuid, args[1] #, kwargs=eval(sys.argv[2])[1]
         )
     else:
         future = gce.submit_to_registered_function(
